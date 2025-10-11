f9f67ac127eebb039640d6388ce58849ed724724
DESKTOP-B5HI9JS\hp <heshamhamdan51@gmail.com>
2025-10-11 11:52:16 +0300

    Add Firebase Core dependency and initialize in main application
    - Added firebase_core dependency to pubspec.yaml for Firebase integration.
    - Updated pubspec.lock to include firebase_core and related platform interfaces.
    - Initialized Firebase in the main function of main.dart to set up Firebase services.
    - Registered Firebase Core plugin in both macOS and Windows platform files for cross-platform support.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
+import 'package:hodorak/firebase_options.dart';
 import 'core/core.dart';
 
 /// Main entry point of the application
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppInitializationService.initialize();
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: HodorakApp()));
 }
 
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import app_links
 import connectivity_plus
+import firebase_core
 import flutter_local_notifications
 import geolocator_apple
 import local_auth_darwin
@@ -17,6 +18,7 @@ import url_launcher_macos
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
